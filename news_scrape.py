45f2524b3c4a5290d98dc063ca9c7a06ac075b0d
Ali Bazrkar <bazrkar138484@gmail.com>
2024-09-13 02:11:24 +0330

    Creating File

--- news_scrape.py
+++ news_scrape.py
@@ -1,4 +1,3 @@
-import time
 from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,9 +6,34 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 
-all_news_list = []
 
-# TODO : replace "\u200c" with " " in title
+def time_cleaner(time):
+
+    if "ساعت پیش" in time:
+        time = time.replace("ساعت پیش", "").strip()
+        return datetime.now() - timedelta(hours=time)
+
+    elif "دقیقه پیش" in time:
+        time = time.replace("دقیقه پیش", "").strip()
+        return datetime.now() - timedelta(minutes=time)
+
+    elif "روز پیش" in time:
+        time = time.replace("روز پیش", "").strip()
+        return datetime.now() - timedelta(days=time)
+
+    elif "ثانیه پیش" in time:
+        time = time.replace("ثانیه پیش", "").strip()
+        return datetime.now() - timedelta(seconds=time)
+
+
+def text_cleaner(text):
+    if r"\u200c" in text:
+        return text.replace(r"\u200c", " ")
+    else:
+        return text
+
+
+all_news_list = []
 
 url = "https://www.shahrekhabar.com"
 
@@ -36,3 +60,9 @@ for group in ["اخبار-ورزشی", "اخبار-سیاسی", "اخبار-اق
             all_news_list.append(news_dict)
 
 driver.quit()
+
+df = pd.DataFrame(all_news_list)
+
+df["زمان"] = df["زمان"].apply(view_cleaner)
+
+df.to_excel("news.xlsx", index=False)
